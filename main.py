8ad5d74f4445c7a5d5d2e6fd430acdc51ffca281
Albashish <49641729+Albashish@users.noreply.github.com>
2023-05-01 09:02:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,44 +3,39 @@ from bs4 import BeautifulSoup
 import json
 
 def FindAlternateGroups(store_domain):
-    # Make a request to the product data URL
-    url = f"{store_domain}/collections/all/products.json?page=1"
-    response = requests.get(url)
-    data = response.json()
+    page = 1
+    products = []
+    while page==1:
+        url = f"{store_domain}/collections/all/products.json?page={page}"
+        response = requests.get(url)
+        data = response.json()
+        if len(data['products']) == 0:
+            break
+        products += data['products']
+        page += 1
 
-    # Create a dictionary to store product alternates
-    alternates = {}
-
-    # Iterate over each product in the data
+    # Create a dictionary of body_html content with simplified text content
+    html_dict = {}
     for product in data['products']:
-        
-        # Get the product link and its variant options
-        product_link = f"{store_domain}/products/{product['handle']}"
-        
-        # Create a unique key for the alternate group using vendor, product_type and tags associated with the product
-        if len(product['tags'])!=0:
-            key=f"{product['product_type']}: {product['tags'][0]}: {product['vendor']} "
-        else:
-            key=f"{product['product_type']}: : {product['vendor']} "
+        soup = BeautifulSoup(product['body_html'], 'html.parser')
+        text = soup.get_text().strip()
+        #print(html_dict.get(text, []) + [product['handle']])
+        html_dict[text] = html_dict.get(text, []) + [product['handle']]
 
-        print(key)
-        
-        # Add the product link to the corresponding alternate group
-        if key in alternates:
-            alternates[key].append(product_link)
-        else:
-            alternates[key] = [product_link]
+    # Find alternate groups
+    alternate_groups = []
+    for handles in html_dict.values():
+        if len(handles) > 1:
+            alternate_groups.append({"product alternates": [f"https://{store_domain}/products/{handle}" for handle in handles]})
+    
 
-    # Convert the alternates dictionary to JSON format
-    result = []
-    for key, value in alternates.items():
-        result.append({"product alternates": value})
+    return json.dumps(alternate_groups, indent=4)
 
-    return json.dumps(result, indent=4)
 
-# Test the function with the provided store domain
+
 store_domain = "https://boysnextdoor-apparel.co"
-alternate_groups = FindAlternateGroups(store_domain)
+alternates = FindAlternateGroups(store_domain)
+print(alternates)
 with open("output.json",'w') as f:
-    f.write(alternate_groups)
+    f.write(alternates)
 
